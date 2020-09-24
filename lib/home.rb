64c88dd4c5fa3d362ae80d772fb8dcf495bca65a
Anna Moignard <amoignard.coder@gmail.com>
2020-09-24 11:49:50 +1000

    I installed bundler, and added terminal-table, tty-prompt and colorize Ruby gems as I plan on using these throughout the terminal app

---
+++ lib/home.rb
@@ -0,0 +1,6 @@
+# welcome to the mood tracker => get user's name 
+puts "Welcome to The Good Luck Generator!"
+puts "Please enter your name!"
+print "> "
+username = gets.chomp
+puts "Thank you #{username}, it's lovely to have you here!"
