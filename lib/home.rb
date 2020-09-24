bcdc43075d9d9a30b563c6ac806ae62d2936eb3a
Anna Moignard <amoignard.coder@gmail.com>
2020-09-24 13:33:52 +1000

    Created a welcome_user method, that gets user name and welcome's the user to the app

--- lib/home.rb
+++ lib/home.rb
@@ -1,6 +1,22 @@
 # welcome to the mood tracker => get user's name 
-puts "Welcome to The Good Luck Generator!"
-puts "Please enter your name!"
-print "> "
-username = gets.chomp
-puts "Thank you #{username}, it's lovely to have you here!"
+class User
+  attr_accessor :username
+
+  def initialize(username)
+    @username = username #should this be gets.chomp as well?
+    # self.welcome_user
+  end
+  
+  private #this means no one else can access 
+  def self.welcome_user
+      puts "Welcome to The Good Luck Generator!"
+      puts "Please enter your name!"
+      print "> "
+      username = gets.chomp
+      puts "Thank you #{username}, it's lovely to have you here!"
+  end
+  welcome_user
+end 
+
+
+# username = Welcome.new(username)
